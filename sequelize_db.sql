3b949aa7e20daa1f88502737807afa04d228afaa
rene <rlatprnr@gmail.com>
2018-09-18 00:49:41 -0400

    updated db file

--- sequelize_db.sql
+++ sequelize_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 18, 2018 at 04:40 AM
+-- Generation Time: Sep 18, 2018 at 04:49 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -29,15 +29,16 @@ USE `sequelize_db`;
 --
 
 DROP TABLE IF EXISTS `post`;
-CREATE TABLE `post` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `responsesCount` int(11) NOT NULL,
   `postTitle` varchar(255) NOT NULL,
   `postContent` text NOT NULL,
   `createdAt` date DEFAULT NULL,
-  `updatedAt` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `post`
@@ -45,7 +46,6 @@ CREATE TABLE `post` (
 
 INSERT INTO `post` (`id`, `username`, `responsesCount`, `postTitle`, `postContent`, `createdAt`, `updatedAt`) VALUES
 (1, 'Jay', 3, 'First post', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\nWhy do we use it?\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n\r\n', '2018-09-11', '2018-09-18'),
-(8, 'granjahoras', 10, '123', 'sf', '2018-09-18', '2018-09-18'),
 (9, 'ewr', 1, 'q234', 'q324', '2018-09-18', '2018-09-18');
 
 -- --------------------------------------------------------
@@ -55,14 +55,15 @@ INSERT INTO `post` (`id`, `username`, `responsesCount`, `postTitle`, `postConten
 --
 
 DROP TABLE IF EXISTS `response`;
-CREATE TABLE `response` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `response` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `postId` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `responseContent` text NOT NULL,
   `createdAt` date DEFAULT NULL,
-  `updatedAt` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `response`
@@ -73,38 +74,6 @@ INSERT INTO `response` (`id`, `postId`, `username`, `responseContent`, `createdA
 (6, 3, 'Yuri', '123123123', '2018-09-18', '2018-09-18'),
 (26, 1, 'Tester', 'sdf aewr', '2018-09-18', '2018-09-18'),
 (28, 9, 'Tester1', 'asdewr', '2018-09-18', '2018-09-18');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `post`
---
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `response`
---
-ALTER TABLE `response`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `response`
---
-ALTER TABLE `response`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
